bb8caec7a7eeaf4e6d5be7ff7446e990c0af8b0e
Lokesh Acharya <lokeshacharya.it22@gmail.com>
2026-08-05 02:11:16 +0530

    First Commit

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__, template_folder='../templates')  # 👈 force template path
+
+app.secret_key = os.getenv("SECRET_KEY")
+
+# MySQL Config
+app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
+app.config['MYSQL_PORT'] = int(os.getenv("MYSQL_PORT", 3306))
+app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
+app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
+app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
+
+print("MYSQL_HOST =", app.config["MYSQL_HOST"])
+print("MYSQL_PORT =", app.config["MYSQL_PORT"])
+print("MYSQL_USER =", app.config["MYSQL_USER"])
+print("MYSQL_DB =", app.config["MYSQL_DB"])
+
+mysql = MySQL(app)
+
+from app import routes
