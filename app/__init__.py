83b4e040c5499b945a9e8a42bff8b60265501495
Lokesh Acharya <lokeshacharya.it22@gmail.com>
2026-08-05 02:44:32 +0530

    updated

--- app/__init__.py
+++ app/__init__.py
@@ -16,11 +16,6 @@ app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
 app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
 app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
 
-print("MYSQL_HOST =", app.config["MYSQL_HOST"])
-print("MYSQL_PORT =", app.config["MYSQL_PORT"])
-print("MYSQL_USER =", app.config["MYSQL_USER"])
-print("MYSQL_DB =", app.config["MYSQL_DB"])
-
 mysql = MySQL(app)
 
 from app import routes
